8cbc923c5806308aa77fde057e9cdf77de675c80
nanorissy <israel@alstergh.com>
2022-03-20 16:51:01 +0000

    sample crud operations with node and mongodb

---
+++ server.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+const connect = mongoose.connect("mongodb://localhost:27017/books");
+connect.then((db) =>{
+    console.log('connected to mongo server')
+}, (error) => console.log(error))
+
+app.use(express.json())
+
+const bookShema = mongoose.Schema({
+    title: String,
+    author: String
+})
+
+const Book = mongoose.model('Book', bookShema)
+
+app.post('/book', (req, res) => {
+
+    Book.create(req.body)
+    .then((book) => {
+        res.status(201).json(book);
+    })
+    .catch(error => console.log(error))
+
+});
+
+app.get('/books', (req, res) => {
+    Book.find({})
+    .then(books => {
+        res.status(200).json(books);
+
+    })
+    .catch(error => console.log(error))
+});
+
+app.get('/book/:bookId', (req, res) => {
+    const {bookId} = req.params;
+
+    Book.findById(bookId)
+    .then(books => {
+        res.status(200).json(books);
+
+    })
+    .catch(error => console.log(error))
+});
+
+app.put('/book/:bookId', (req, res) => {
+    const {bookId} = req.params;
+
+    Book.findByIdAndUpdate(bookId, req.body)
+    .then(books => {
+        res.status(200).json(books);
+
+    })
+    .catch(error => console.log(error))
+});
+
+app.delete('/book/:bookId', (req, res) => {
+    const {bookId} = req.params;
+
+    Book.findByIdAndDelete(bookId)
+    .then(books => {
+        res.status(200).json(books);
+
+    })
+    .catch(error => console.log(error))
+});
+
+app.listen(5000, () => {
+    console.log('server running successfully')
+})
